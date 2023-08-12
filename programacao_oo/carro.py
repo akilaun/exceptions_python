a9b1bb0af2668be6f2eff88836ed6a7e6ac97925
Akila <akilaun@gmail.com>
2023-08-12 09:18:27 -0300

    main

---
+++ programacao_oo/carro.py
@@ -0,0 +1,9 @@
+class Carro:
+    
+    def __init__(self):
+        self.marca = "Volkswagem"
+        self.modelo = "Gol 1.6"
+        self.cor = "Branco"
+        self.tem_rodas = False
+        self.ano_fabricacao = 1999
+
