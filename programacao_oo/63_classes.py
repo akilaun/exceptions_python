f458333962dd2425f0c678cd4ba428dfa477e4bd
Akila <akilaun@gmail.com>
2023-08-12 09:41:14 -0300

    main

---
+++ programacao_oo/63_classes.py
@@ -0,0 +1,8 @@
+from carro import Carro
+
+carro_gol = Carro() 
+print (f"O carro {carro_gol.modelo} \n"
+       f"é da marca: {carro_gol.marca} \n"
+       f"com a cor: {carro_gol.cor} \n"
+       f"ano de fabricação: {carro_gol.ano_fabricacao} \n"
+       f"tem rodas: {carro_gol.tem_rodas} \n")
